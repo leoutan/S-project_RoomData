a2e79b0b6b6a3e1740b0f131823abb9d80fe5050
Leo Tseng <leo81xi16@gmail.com>
2022-09-14 15:29:35 +0800

    2_agoda_add BeautifulSoup

--- selenium_openbrower.py
+++ selenium_openbrower.py
@@ -8,6 +8,7 @@ import time
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
 
 #開啟Chrome瀏覽器
 browser = webdriver.Chrome(ChromeDriverManager().install())
@@ -25,10 +26,11 @@ try:
   close.click()
 
 finally:
-  #定位地點
+  #定位地點並輸入墾丁
   place = browser.find_element(By.XPATH,'//input[@aria-label="輸入城市、區域、景點或住宿名稱..."]')
   place.send_keys(u'墾丁')
   
+  #等到下拉選單出現，就點選第一項地點
   first = WebDriverWait(browser, 10).until(
     EC.presence_of_element_located((By.XPATH, "//li[@class='Suggestion Suggestion__categoryName']"))
   )
@@ -37,30 +39,43 @@ finally:
   #select_dates = browser.find_element(By.XPATH,"//div[@data-selenium='checkInBox']")
   #select_dates.click()
 
-
-
+  #點選地點的下拉選單後，等到地點的下拉選單消失
+  #網頁會自動跑出日曆，就能定位到入住日期，不必自己點選日曆
+  #接著點選入住日期19號
   start_date = WebDriverWait(browser, 10).until(
     EC.presence_of_element_located((By.XPATH,"//div//*[text()='19']"))
   )
   start_date.click()
+
+  #退房日期23號
+  #因為elements用複數，click()前面加陣列選取第二項
   end_date = WebDriverWait(browser, 10).until(
     EC.presence_of_all_elements_located((By.XPATH,"//div//*[text()='23']"))
   )
-  end_date[1].click()
+  end_date[0].click()
+
+  #如果選擇人下拉選單自己跳出來就執行finally
+  #如果選單沒出現就執行except
   try:
     Select_traveller = WebDriverWait(browser, 10).until(
       EC.presence_of_element_located((By.XPATH,'//div[@class="Traveller"]'))
     )
+
+  #點選旅遊人數跑出下拉選單
   except:
     Select_traveller = WebDriverWait(browser, 10).until(
       EC.presence_of_element_located((By.XPATH,'//div[@data-selenium="occupancyBox"]'))
     )
     Select_traveller.click()
+
+  #從下拉選單中選擇單人旅遊
   finally:
     travellers = WebDriverWait(browser, 10).until(
       EC.presence_of_element_located((By.XPATH,'//div[@data-selenium="traveler-solo"]'))
     )
     travellers.click()
+  
+  #定位並點選搜尋按鈕
   search = browser.find_element(By.XPATH, '//button[@data-selenium="searchButton"]')
   search.click()
   dont = WebDriverWait(browser,10).until(
@@ -68,6 +83,17 @@ finally:
   )
   dont.click()
 
+time.sleep(10)
+lists = WebDriverWait(browser, 10).until(
+  EC.presence_of_element_located((By.XPATH,'//h3[@class="PropertyCard__HotelName"]'))
+)
 result_url = browser.current_url
-
-    
+#print(result_url)
+#response = requests.get(result_url)
+soup = BeautifulSoup(browser.page_source, 'html5lib')
+js = "window.scrollTo(0, document.body.scrollHeight);"
+browser.execute_script(js)
+HotelNames = soup.find_all('h3', class_='PropertyCard__HotelName')
+#print(soup)
+for HotelName in HotelNames:
+  print(HotelName.get_text())  
