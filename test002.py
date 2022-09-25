c9986d5339e495c7959bf79cdbc3de83e4efbc57
Leo Tseng <leo81xi16@gmail.com>
2022-09-25 12:17:02 +0800

    tryagain

--- test002.py
+++ test002.py
@@ -2,14 +2,6 @@ from email.utils import localtime
 import imp
 from numpy import broadcast
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import time
-from selenium.webdriver.common.by import By
-from bs4 import BeautifulSoup
-import requests
-import pandas as pd
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
   
